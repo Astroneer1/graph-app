8c41fc80b4443be5762e60bf62b74a96d69aefc5
Astroneer1 <15jy0124@gmail.com>
2021-07-19 02:43:14 +0900

    Error fix

--- app/graphql/mutations/delete_post.rb
+++ app/graphql/mutations/delete_post.rb
@@ -4,7 +4,7 @@ module Mutations
 
     field :post, ObjectTypes::PostType, null: false
 
-    def resolve(id: id)
+    def resolve(id:)
       post = Post.find(id).destroy
 
       { post: post }
--- app/graphql/resolvers/posts.rb
+++ app/graphql/resolvers/posts.rb
@@ -1,8 +1,8 @@
 module Resolvers
   class Posts < Resolvers::BaseResolver
-    type ObjectTypes::PostType, null: false
     argument :users_id, Integer, required: true
-    # field :post, ObjectTypes::PostType, null: false
+
+    type ObjectTypes::PostType, null: false
 
     def resolve(users_id:)
       Post.find_by(users_id: users_id)

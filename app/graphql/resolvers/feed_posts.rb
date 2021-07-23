4bec3a6cfc7e4639b7bfba39202ff4c76252d3a8
Astroneer1 <15jy0124@gmail.com>
2021-07-23 21:23:23 +0900

    refactoring

--- app/graphql/resolvers/feed_posts.rb
+++ app/graphql/resolvers/feed_posts.rb
@@ -1,14 +1,13 @@
 module Resolvers
-    class FeedPosts < Resolvers::BaseResolver
-      argument :user_id, ID, loads: ObjectTypes::UserType, required: true
-  
-      type [ObjectTypes::PostType], null: false
-  
-      def resolve(user:)
-        user_ids = Following.where(user_id: user.id).pluck(:following_user_id)
-        user_ids.push(user.id)
-        Post.where(user_id: user_ids).order(created_at: "DESC")
-      end
+  class FeedPosts < Resolvers::BaseResolver
+    argument :user_id, ID, loads: ObjectTypes::UserType, required: true
+
+    type [ObjectTypes::PostType], null: false
+
+    def resolve(user:)
+      user_ids = Following.where(user_id: user.id).pluck(:following_user_id)
+      user_ids.push(user.id)
+      Post.where(user_id: user_ids).order(created_at: 'DESC')
     end
   end
-  
+end

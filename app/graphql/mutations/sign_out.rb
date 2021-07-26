e914bbbfa967111dfbea8faef8bb2d3505e5d7b9
Astroneer1 <15jy0124@gmail.com>
2021-07-26 18:35:14 +0900

    refactoring

--- app/graphql/mutations/sign_out.rb
+++ app/graphql/mutations/sign_out.rb
@@ -2,7 +2,9 @@ module Mutations
   class SignOut < BaseMutation
     def resolve
       api_token = context[:current_user]
-      ApiToken.find_by(user_id: api_token.user_id, token: api_token.token).destroy
+      seach_api_token = ApiToken.find_by(user_id: api_token.user_id, token: api_token.token)
+
+      seach_api_token.destroy if seach_api_token.nil?
 
       nil
     end
